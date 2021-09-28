1ea02c23785f62699f195dde9d5b58eb8e92bcb1
xuri02 <t0bl4s.durst@gmail.com>
2021-09-28 13:30:22 +0200

    anged changing observer and extensions to be more spesifi

--- src/Ajiva.Include/Utils/Changing/ChangingObserver.cs
+++ src/Ajiva.Include/Utils/Changing/ChangingObserver.cs
@@ -1,8 +1,123 @@
-﻿namespace ajiva.Utils.Changing
+﻿using System;
+
+namespace ajiva.Utils.Changing
 {
     public class ChangingObserver : IChangingObserver
     {
-        public ChangingObserver(int delayUpdateFor)
+        public ChangingObserver(int changeThreshold)
+        {
+            ChangeThreshold = changeThreshold;
+        }
+
+        /// <inheritdoc />
+        public int ChangeThreshold { get; }
+
+        /// <inheritdoc />
+        public int ChangedAmount { get; set; } = 0;
+
+        private object _lock { get; } = new();
+
+        /// <inheritdoc />
+        public event OnChangedDelegate? OnChanged;
+
+        /// <inheritdoc />
+        public void Changed()
+        {
+            lock (_lock)
+            {
+                ChangedAmount++;
+                if (ChangedAmount > ChangeThreshold)
+                {
+                    ChangedAmount = 0;
+
+                    OnChanged?.Invoke(this);
+                }
+            }
+        }
+    }
+    public class ChangingObserverOnlyAfter<TSender, TValue> : IChangingObserverOnlyAfter<TSender, TValue> where TSender : class where TValue : struct
+    {
+        public ChangingObserverOnlyAfter(TSender owner, Func<TValue> result, int changeThreshold)
+        {
+            Owner = owner;
+            Result = result;
+            ChangeThreshold = changeThreshold;
+        }
+
+        public TSender Owner { get; }
+
+        /// <inheritdoc />
+        public Func<TValue> Result { get; }
+
+        /// <inheritdoc />
+        public int ChangeThreshold { get; }
+
+        /// <inheritdoc />
+        public int ChangedAmount { get; set; }
+
+        /// <inheritdoc />
+        public event IChangingObserverOnlyAfter<TSender, TValue>.OnChangedDelegate? OnChanged;
+
+        /// <inheritdoc />
+        public void Changed(TValue after)
+        {
+            lock (_lock)
+            {
+                ChangedAmount++;
+                if (ChangedAmount > ChangeThreshold)
+                {
+                    ChangedAmount = 0;
+                    OnChanged?.Invoke(Owner, after);
+                }
+            }
+        }
+
+        private object _lock { get; } = new();
+    }
+    public class ChangingObserver<TSender, TValue> : IChangingObserver<TSender, TValue> where TSender : class where TValue : struct
+    {
+        public ChangingObserver(TSender owner, Func<TValue> result, int changeThreshold)
+        {
+            Owner = owner;
+            Result = result;
+            ChangeThreshold = changeThreshold;
+        }
+
+        public TSender Owner { get; }
+
+        /// <inheritdoc />
+        public Func<TValue> Result { get; }
+
+        /// <inheritdoc />
+        public int ChangeThreshold { get; }
+
+        /// <inheritdoc />
+        public int ChangedAmount { get; set; }
+
+        /// <inheritdoc />
+        public event IChangingObserver<TSender, TValue>.OnChangedDelegate? OnChanged;
+
+        /// <inheritdoc />
+        public void Changed(TValue before, TValue after)
+        {
+            lock (_lock)
+            {
+                ChangedAmount++;
+                if (ChangedAmount > ChangeThreshold)
+                {
+                    ChangedAmount = 0;
+
+                    OnChanged?.Invoke(Owner, before, after);
+                }
+            }
+        }
+
+        private object _lock { get; } = new();
+    }
+
+    public class OverTimeChangingObserver : IOverTimeChangingObserver
+    {
+        public OverTimeChangingObserver(int delayUpdateFor)
         {
             DelayUpdateFor = delayUpdateFor;
         }
@@ -19,10 +134,10 @@
         private object _lock { get; } = new();
 
         /// <inheritdoc />
-        public event OnChangedDelegate? OnChanged;
+        public event IOverTimeChangingObserver.OnChangedDelegate? OnChanged;
 
         /// <inheritdoc />
-        public event OnUpdateDelegate? OnUpdate;
+        public event IOverTimeChangingObserver.OnUpdateDelegate? OnUpdate;
 
         /// <inheritdoc />
         public void Changed()
@@ -43,6 +158,57 @@
                 ChangeBeginCycle = 0;
             }
             OnUpdate?.Invoke(this);
+        }
+
+        public IDisposable BeginBigChange()
+        {
+            BigChangeCount++;
+            Current ??= new BachChange(this);
+            return Current;
+        }
+
+        public void EndBigChange()
+        {
+            BigChangeCount--;
+            if (BigChangeCount <= 0)
+            {
+                BigChangeCount = 0;
+                if (Current is not null)
+                {
+                    if (ChangedAmount > Current.BeginChange)
+                    {
+                        ChangeBeginCycle = Current.BeginCycle; // - DelayUpdateFor ?
+                    }
+                    Current = null;
+                }
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Locked => Current is not null;
+
+        public int BigChangeCount { get; set; }
+        public BachChange? Current { get; set; }
+        public class BachChange : DisposingLogger
+        {
+            /// <inheritdoc />
+            public BachChange(IOverTimeChangingObserver parent)
+            {
+                Parent = parent;
+                BeginCycle = parent.ChangeBeginCycle;
+                BeginChange = parent.ChangedAmount;
+            }
+
+            public long BeginCycle { get; set; }
+            public int BeginChange { get; }
+            private IOverTimeChangingObserver Parent { get; }
+
+            /// <inheritdoc />
+            protected override void ReleaseUnmanagedResources(bool disposing)
+            {
+                base.ReleaseUnmanagedResources(disposing);
+                Parent.EndBigChange();
+            }
         }
     }
 }
--- src/Ajiva.Include/Utils/Changing/IChangingObserver.cs
+++ src/Ajiva.Include/Utils/Changing/IChangingObserver.cs
@@ -1,6 +1,40 @@
-﻿namespace ajiva.Utils.Changing
+﻿using System;
+
+namespace ajiva.Utils.Changing
 {
+    public interface IChangingObserver<TSender, TValue> where TSender : class where TValue : struct
+    {
+        TSender Owner { get; }
+        Func<TValue> Result { get; }
+        int ChangeThreshold { get; }
+        int ChangedAmount { get; set; }
+        event OnChangedDelegate OnChanged;
+        void Changed(TValue before, TValue after);
+
+        public delegate void OnChangedDelegate(TSender sender, TValue before, TValue after);
+    }
+    public interface IChangingObserverOnlyAfter<TSender, TValue> where TSender : class where TValue : struct
+    {
+        TSender Owner { get; }
+        Func<TValue> Result { get; }
+        int ChangeThreshold { get; }
+        int ChangedAmount { get; set; }
+        event OnChangedDelegate OnChanged;
+        void Changed(TValue after);
+
+        public delegate void OnChangedDelegate(TSender sender, TValue after);
+    }
     public interface IChangingObserver
+    {
+        int ChangeThreshold { get; }
+
+        int ChangedAmount { get; set; }
+
+        event OnChangedDelegate OnChanged;
+
+        void Changed();
+    }
+    public interface IOverTimeChangingObserver
     {
         int DelayUpdateFor { get; }
 
@@ -12,6 +46,16 @@
 
         void Changed();
         void Updated();
+
+        public delegate void OnUpdateDelegate(IOverTimeChangingObserver sender);
+
+        public delegate void OnChangedDelegate(IOverTimeChangingObserver sender);
+
+        IDisposable BeginBigChange();
+
+        void EndBigChange();
+        
+        bool Locked { get;  }
     }
 
     public delegate void OnUpdateDelegate(IChangingObserver sender);
